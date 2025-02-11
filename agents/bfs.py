10d19a89d55db6dd064fee67611a67ad8b03c05b
Amirhossein Ghanbari <ameerduh@yahoo.com>
2025-02-11 21:13:05 +0330

    DFS & BFS Agents

--- agents/bfs.py
+++ agents/bfs.py
@@ -1,13 +1,28 @@
-from funcs import *
+from funcs import is_legal, move, check_winning, is_blocked
+from copy import deepcopy
 
-steps = ((0, 1), # Down
-        (1, 0), # Right
-        (0, -1), # Up
-        (-1, 0)) # Left
+steps = ((0, 1), (1, 0), (0, -1), (-1, 0))  # Down, Right, Up, Left
+
 
 def bfs(game_state):
-    '''
-    - TODO: YOUR BFS AGENT CODE MUST BE WRITTEN HERE
-    - Returns: Final GameState of the solution
-    '''
-    return game_state # For Example Purposes
+    queue = [game_state]
+    visited = []
+
+    while queue:
+        current_state = queue.pop(0)
+
+        if check_winning(current_state):
+            return current_state
+
+        state_visited = [current_state.player_x, current_state.player_y, current_state.grid]
+        if state_visited in visited:
+            continue
+        visited.append(state_visited)
+
+        for step in steps:
+            next_state = deepcopy(current_state)
+            if is_legal(next_state, step) and not is_blocked(next_state, step):
+                move(next_state, step)
+                queue.append(next_state)
+
+    return game_state
--- agents/dfs.py
+++ agents/dfs.py
@@ -1,13 +1,28 @@
-from funcs import *
+from funcs import is_legal, move, check_winning, is_blocked
+from copy import deepcopy
 
-steps = ((0, 1), # Down
-        (1, 0), # Right
-        (0, -1), # Up
-        (-1, 0)) # Left
+steps = ((0, 1), (1, 0), (0, -1), (-1, 0))  # Down, Right, Up, Left
+
 
 def dfs(game_state):
-    '''
-    - TODO: YOUR DFS AGENT CODE MUST BE WRITTEN HERE
-    - Returns: Final GameState of the solution
-    '''
-    return game_state # For Example Purposes
+    stack = [game_state]
+    visited = []
+
+    while stack:
+        current_state = stack.pop()
+
+        if check_winning(current_state):
+            return current_state
+
+        state_visited = [current_state.player_x, current_state.player_y, current_state.grid]
+        if state_visited in visited:
+            continue
+        visited.append(state_visited)
+
+        for step in steps:
+            next_state = deepcopy(current_state)
+            if is_legal(next_state, step) and not is_blocked(next_state, step):
+                move(next_state, step)
+                stack.append(next_state)
+
+    return game_state
